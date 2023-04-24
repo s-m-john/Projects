060ba772e0ad71b47344019241d28be9840661ad
s-m-john <107650644+s-m-john@users.noreply.github.com>
2023-04-24 19:32:43 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Storefront from './components/Storefront';
+
+function App() {
+  return (
+    <div className="App">
+      <Storefront />
+    </div>
+  );
+}
+
+export default App;
